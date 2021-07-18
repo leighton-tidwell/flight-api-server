12a81665368144303259cbc82c4adcbd2738384e
Leighton Tidwell <leighton.tidwell57@gmail.com>
2021-07-18 14:54:29 -0400

    Add CORS policy

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const db = require("./database");
 app.use(cors());
 app.use(mongoSanitize());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/api/search/:search", (req, res) => {
   db.airports
     .find(
@@ -21,7 +30,7 @@ app.get("/api/search/:search", (req, res) => {
 
 app.get("/api/faa/:faa", (req, res) => {
   db.airports
-    .find({ 'ident': `${req.params.faa}` })
+    .find({ ident: `${req.params.faa}` })
     .exec()
     .then((results) => res.json(results));
 });
