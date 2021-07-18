c7f8eac81cd04acbc1470b8b828856be582380c2
Leighton Tidwell <leighton.tidwell57@gmail.com>
2021-07-18 15:06:18 -0400

    Add Middleware for security

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const morgan = require("morgan");
 const app = express();
 const port = 8000;
 const db = require("./database");
 
 app.use(cors());
 app.use(mongoSanitize());
+app.use(helmet());
+app.use(morgan("combined"));
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
+});
+
+app.get("/", (req, res) => {
+  res.json({ Paths: ["/api/search:search", "/api/faa/:faa"] });
 });
 
 app.get("/api/search/:search", (req, res) => {
